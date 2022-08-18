343cbe2dc1cc580d2feae9154c5e9d6737eb3ce6
Licon <1163760935@qq.com>
2022-08-18 22:40:16 +0800

    init

---
+++ src/typings/utils.d.ts
@@ -0,0 +1,13 @@
+declare namespace TypeUtil {
+  type Noop = (...args: any) => any;
+
+  type UnionInclude<T, K extends keyof T> = K extends keyof T ? true : false;
+
+  type GetFunArgs<F extends Noop> = F extends (...args: infer P) => any ? P : never;
+
+  type Writable<T> = { [K in keyof T]: T[K] };
+
+  type FirstOfArray<T extends any[]> = T extends [infer First, ...infer _Rest] ? First : never;
+
+  type LastOfArray<T extends any[]> = T extends [...infer _Rest, infer Last] ? Last : never;
+}
